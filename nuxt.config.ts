1265a0b123e5ced4ebec225b35cdda91c097a92f
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2023-10-02 23:26:36 +0700

    feat(dep): fixed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  alias: {
+    pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs'
+  },
   devtools: { enabled: true }
 })
